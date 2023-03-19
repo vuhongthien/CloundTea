8f3520d99a9a68ae9ed1691ef103e83301c809d4
vuhongthien <vuhongthien18@gmail.com>
2023-03-19 19:14:43 +0700

    update enviroment

---
+++ cloudtea/src/main/java/com/service/cloudtea/CloudteaApplication.java
@@ -0,0 +1,13 @@
+package com.service.cloudtea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudteaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudteaApplication.class, args);
+	}
+
+}
---
+++ cloudtea/src/test/java/com/service/cloudtea/CloudteaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.service.cloudtea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudteaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
