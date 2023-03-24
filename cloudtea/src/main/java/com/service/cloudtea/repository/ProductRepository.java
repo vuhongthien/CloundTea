45738da03dcff83b234ef456132938db10b37149
vuhongthien <vuhongthien18@gmail.com>
2023-03-25 00:41:39 +0700

    update to deploy api type product

--- cloudtea/src/main/java/com/service/cloudtea/repository/ProductRepository.java
+++ cloudtea/src/main/java/com/service/cloudtea/repository/ProductRepository.java
@@ -2,6 +2,8 @@ package com.service.cloudtea.repository;
 
 import com.service.cloudtea.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
--- cloudtea/src/main/java/com/service/cloudtea/repository/ProductTypeRepository.java
+++ cloudtea/src/main/java/com/service/cloudtea/repository/ProductTypeRepository.java
@@ -2,6 +2,8 @@ package com.service.cloudtea.repository;
 
 import com.service.cloudtea.model.ProductType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductTypeRepository extends JpaRepository<ProductType, Long> {
 }
